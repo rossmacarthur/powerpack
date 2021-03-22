27cf7d9209a225d0e82ce4575d5428b914ca6a5f
Ross MacArthur <ross@macarthur.io>
2021-03-22 21:21:19 +0200

    Implement serialization

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
 use std::collections::HashMap;
 use std::path::PathBuf;
 
+use serde::ser::SerializeStruct;
+use serde::{Serialize, Serializer};
+
+fn is_default<T: Default + PartialEq>(t: &T) -> bool {
+    t == &T::default()
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // Definitions
 ////////////////////////////////////////////////////////////////////////////////
@@ -9,17 +16,22 @@ use std::path::PathBuf;
 pub type String<'a> = beef::lean::Cow<'a, str>;
 
 /// A keyboard modifier.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize)]
 pub enum ModifierKey {
     /// ⌘
+    #[serde(rename = "cmd")]
     Command,
     /// ⌥
+    #[serde(rename = "alt")]
     Option,
     /// ⌃
+    #[serde(rename = "ctrl")]
     Control,
     /// ⇧
+    #[serde(rename = "shift")]
     Shift,
     /// fn
+    #[serde(rename = "fn")]
     Function,
 }
 
@@ -35,73 +47,146 @@ pub enum Icon<'a> {
 }
 
 /// The type of item.
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize)]
 pub enum Kind {
+    #[serde(rename = "default")]
     Default,
+    #[serde(rename = "file")]
     File,
+    #[serde(rename = "file:skipcheck")]
     FileSkipCheck,
 }
 
 /// The copied or large type text.
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize)]
 pub struct Text<'a> {
     /// Defines the text the user will get when copying the item (⌘+C).
     copy: Option<String<'a>>,
+
     /// Defines the text the user will see in large type (⌘+L).
+    #[serde(rename = "largetype")]
     large_type: Option<String<'a>>,
 }
 
 /// The settings for when a modifier key is pressed.
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize)]
 pub struct ModifierData<'a> {
     /// The subtitle displayed in the result row.
+    #[serde(skip_serializing_if = "Option::is_none")]
     subtitle: Option<String<'a>>,
+
     /// The argument which is passed through to the output.
+    #[serde(skip_serializing_if = "Option::is_none")]
     arg: Option<String<'a>>,
+
     /// The icon displayed in the result row when the modifier is pressed.
+    #[serde(skip_serializing_if = "Option::is_none")]
     icon: Option<Icon<'a>>,
+
     /// Mark whether the item is valid when the modifier is pressed.
+    #[serde(skip_serializing_if = "Option::is_none")]
     valid: Option<bool>,
 }
 
 /// An Alfred script filter item.
-#[derive(Debug, Clone, Default, PartialEq, Eq)]
+#[derive(Debug, Clone, Default, PartialEq, Eq, Serialize)]
 pub struct Item<'a> {
     /// The title displayed in the result row.
     title: String<'a>,
+
     /// The subtitle displayed in the result row.
+    #[serde(skip_serializing_if = "Option::is_none")]
     subtitle: Option<String<'a>>,
+
     /// A unique identifier for the item.
+    #[serde(skip_serializing_if = "Option::is_none")]
     uid: Option<String<'a>>,
+
     /// The argument which is passed through to the output.
+    #[serde(skip_serializing_if = "Option::is_none")]
     arg: Option<String<'a>>,
+
     /// The icon displayed in the result row.
+    #[serde(skip_serializing_if = "Option::is_none")]
     icon: Option<Icon<'a>>,
+
     /// Whether this item is valid or not.
+    #[serde(skip_serializing_if = "Option::is_none")]
     valid: Option<bool>,
+
     /// Enables you to define what Alfred matches against.
+    #[serde(rename = "match", skip_serializing_if = "Option::is_none")]
     matches: Option<String<'a>>,
+
     /// Populates the search field when the user auto-completes the result.
+    #[serde(skip_serializing_if = "Option::is_none")]
     autocomplete: Option<String<'a>>,
+
     /// The type of item.
+    #[serde(rename = "type", skip_serializing_if = "is_default")]
     kind: Kind,
+
     /// Control how the modifier keys react.
+    #[serde(rename = "mods")]
     modifiers: HashMap<ModifierKey, ModifierData<'a>>,
+
     /// Defines the copied or large type text for this item.
+    #[serde(skip_serializing_if = "Option::is_none")]
     text: Option<Text<'a>>,
+
     /// A Quick Look URL which will be shown if the user uses Quick Look (⌘+Y).
+    #[serde(rename = "quicklookurl", skip_serializing_if = "Option::is_none")]
     quicklook_url: Option<String<'a>>,
 }
 
 /// The output of a workflow (i.e. input for the script filter)
-pub struct Output {
+#[derive(Debug, Clone, Default, PartialEq, Eq, Serialize)]
+pub struct Output<'a> {
     /// Each row item.
-    items: Vec<Item>,
+    items: Vec<Item<'a>>,
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // Implementations
 ////////////////////////////////////////////////////////////////////////////////
+
+impl Serialize for Icon<'_> {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        match self {
+            Self::Path(path) => {
+                let mut s = serializer.serialize_struct("Icon", 1)?;
+                s.serialize_field("path", &path)?;
+                s.end()
+            }
+            Self::File(path) => {
+                let mut s = serializer.serialize_struct("Icon", 2)?;
+                s.serialize_field("type", "fileicon")?;
+                s.serialize_field("path", &path)?;
+                s.end()
+            }
+            Self::FileType(string) => {
+                let mut s = serializer.serialize_struct("Icon", 2)?;
+                s.serialize_field("type", "filetype")?;
+                s.serialize_field("path", &string)?;
+                s.end()
+            }
+        }
+    }
+}
+
+impl<'a> Icon<'a> {
+    pub fn new(path: impl Into<PathBuf>) -> Self {
+        Self::Path(path.into())
+    }
+
+    pub fn new_file(path: impl Into<PathBuf>) -> Self {
+        Self::File(path.into())
+    }
+
+    pub fn new_file_type(path: impl Into<String<'a>>) -> Self {
+        Self::FileType(path.into())
+    }
+}
 
 impl Default for Kind {
     fn default() -> Self {
@@ -114,19 +199,13 @@ macro_rules! setter {
         setter! { $name, Option<String<'a>> }
     };
     ($name:ident, Option<$ty:ty>) => {
-        pub fn $name<V>(mut self, value: V) -> Self
-        where
-            V: Into<$ty>,
-        {
+        pub fn $name(mut self, value: impl Into<$ty>) -> Self {
             self.$name = Some(value.into());
             self
         }
     };
     ($name:ident, $ty:ty) => {
-        pub fn $name<V>(mut self, value: V) -> Self
-        where
-            V: Into<$ty>,
-        {
+        pub fn $name(mut self, value: impl Into<$ty>) -> Self {
             self.$name = value.into();
             self
         }
@@ -143,10 +222,7 @@ impl<'a> Item<'a> {
     ///
     /// let item = Item::new("something");
     /// ```
-    pub fn new<S>(title: S) -> Self
-    where
-        S: Into<String<'a>>,
-    {
+    pub fn new(title: impl Into<String<'a>>) -> Self {
         Self {
             title: title.into(),
             ..Self::default()
@@ -161,5 +237,21 @@ impl<'a> Item<'a> {
     setter! { matches }
     setter! { autocomplete }
     setter! { kind, Kind }
+    setter! { text, Option<Text<'a>> }
     setter! { quicklook_url }
+
+    pub fn modifier(mut self, key: ModifierKey, data: ModifierData<'a>) -> Self {
+        self.modifiers.insert(key, data);
+        self
+    }
+}
+
+impl<'a> Output<'a> {
+    pub fn items<I>(mut self, iter: I) -> Self
+    where
+        I: IntoIterator<Item = Item<'a>>,
+    {
+        self.items = iter.into_iter().collect();
+        self
+    }
 }
