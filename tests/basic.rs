fa11d5eb43f2df1b9c69ff2638cda0452f4b20c5
Ross MacArthur <ross@macarthur.io>
2022-03-05 12:44:42 +0200

    Add basic tests

---
+++ tests/basic.rs
@@ -0,0 +1,50 @@
+use powerpack::{Icon, Item, Kind, ModifierData, ModifierKey, Output};
+
+#[test]
+fn smoke() {
+    let item = Item::new("Desktop")
+        .uid("desktop")
+        .kind(Kind::File)
+        .subtitle("~/Desktop")
+        .arg("~/Desktop")
+        .autocomplete("~/Desktop")
+        .icon(Icon::with_file_icon("~/Desktop"));
+
+    goldie::assert_json!(output([item]));
+}
+
+#[test]
+fn all() {
+    let item = Item::new("Hello world!")
+        .subtitle("This is a subtitle")
+        .uid("unique identifier")
+        .arg("/path/to/file.jpg")
+        .icon(Icon::with_type("public.jpeg"))
+        .valid(true)
+        .matches("use this to filter")
+        .autocomplete("to this")
+        .kind(Kind::FileSkipCheck)
+        .modifier(
+            ModifierKey::Command,
+            ModifierData::new().subtitle("⌘ changes the subtitle"),
+        )
+        .modifier(
+            ModifierKey::Option,
+            ModifierData::new().arg("/path/to/modified.jpg"),
+        )
+        .modifier(
+            ModifierKey::Control,
+            ModifierData::new().icon(Icon::with_image("/path/to/file.png")),
+        )
+        .modifier(ModifierKey::Shift, ModifierData::new().valid(false))
+        .quicklook_url("https://example.com");
+
+    goldie::assert_json!(output([item]));
+}
+
+fn output<'a, I>(items: I) -> Output<'a>
+where
+    I: IntoIterator<Item = Item<'a>>,
+{
+    Output::default().items(items)
+}
